15eb5f9b9d413c14aab7e3a7aad9e3785ba3291d
arshadvkb <arshadvkb118@gmail.com>
2025-10-04 10:48:23 +0530

    view books finished

--- src/pages/home_page.jsx
+++ src/pages/home_page.jsx
@@ -1,13 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../components/Navbar'
 
+
 const Home_page = () => {
+  const [books,setBooks]=useState([])
+const viewBooks=async()=>{
+  try { 
+    const res= await fetch('http://localhost:8000/api/book/view-books')
+    const data= await res.json()
+    
+    setBooks(data.books)
+    console.log(books);
+    
+   
+  }
+  catch (error) {
+    
+  } 
+}
+viewBooks()
   return (
+    
     <div>
       <Navbar/>
-      <h1>home_page</h1>
-    </div>
+      {books.length > 0 ? (
+          <ul>
+            {books.map((book, index) => (
+              <li key={index}> 
+              <div className="ml-2">
+                <h2>{book.title}</h2>
+                <p>Author: {book.author}</p>
+                  </div>
+              </li>
+            
+            ))}
+          </ul>
+        ) : (
+          <p>No books found.</p>
+        )}
+     </div>
+   
   )
+  
+
 }
 
 export default Home_page
