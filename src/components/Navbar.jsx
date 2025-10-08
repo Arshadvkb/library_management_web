6ae510b15cc33d09f99701f958db9ea66c4476fc
arshadvkb <arshadvkb118@gmail.com>
2025-10-08 17:18:01 +0530

    refactor: fix typo in isLoggedIn and improve code formatting in Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,38 @@
-import React, { useContext } from 'react'
-import { AppContext } from '../context/AppContext'
-
+import React, { useContext } from "react";
+import { AppContext } from "../context/AppContext";
+import axios from "axios";
+import { toast } from "react-toastify/unstyled";
 
 const Navbar = () => {
-  const {isLogggedIn}=useContext(AppContext)
+  const { isLoggedIn, setIsLoggedIn, backendurl } = useContext(AppContext);
+  const loggedout=()=> toast("Logged out successfuly")
+
+  const logoutfunction = async () => {
+    const data = await axios.post(backendurl + "/api/auth/logout");
+    if (data.success) {
+      setIsLoggedIn(false);
+      loggedout()
+    }
+  };
   return (
-    <div className='bg-blue-400 h-10 w-auto flex justify-between p-4'>
+    <div className="bg-blue-400 h-10 w-auto flex justify-between p-4">
       <h1>library</h1>
 
-      <ul className='flex justify-between pl-5 gap-3'>
-        <li ><a href="/admin/home">home</a></li>
-      
-        <li ><a href="/">login</a></li>
-        <li ><a href="/add-book">add book</a></li>
-
-       
+      <ul className="flex justify-between pl-5 gap-3">
+        <li>
+          <a href="/admin/home">home</a>
+        </li>
+        <li>
+          <a href="/add-book">add book</a>
+        </li>
+        <li>
+          <a href="/" onClick={logoutfunction}>
+            logout
+          </a>
+        </li>
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
